911e8c226076a3eb561a4041b826012106006730
Sterling Hirsh <mybadluck22@gmail.com>
2021-01-25 04:38:41 -0600

    Path: rework drawing to generate an array of Shapes
    It would be nice to leave Path as Shapes as long as possible before
    converting to Points. This can make transforms easier to apply during
    animations.

--- packages/draw/src/Path.ts
+++ packages/draw/src/Path.ts
@@ -5,10 +5,12 @@ import { Line } from './Line';
 import { Wait } from './Wait';
 import { CubicCurve } from './CubicCurve';
 import { SVGPathData } from 'svg-pathdata';
-import { CommandM, SVGCommand } from 'svg-pathdata/lib/types';
 import { QuadCurve } from './QuadCurve';
 import { flatten } from './helpers';
 import arcToBezier = require('svg-arc-to-cubic-bezier');
+import { CommandM, CommandL, CommandH, CommandV,
+  CommandQ, CommandC, CommandA,
+  SVGCommand } from 'svg-pathdata/lib/types';
 
 interface PathOptions {
   x?: number;
@@ -33,6 +35,10 @@ export class Path extends Shape {
 
   waitAmount: number | undefined;
   blankingAmount: number | undefined;
+
+  private lastMoveCommand: CommandM | undefined;
+  private prevX: number = 0;
+  private prevY: number = 0;
 
   constructor(options: PathOptions) {
     super();
@@ -76,14 +82,254 @@ export class Path extends Shape {
     return command;
   };
 
-  draw(options: SceneOptions): Point[] {
-    const pathData = new SVGPathData(this.path)
+  getPathShapes(): Shape[] {
+    const pathData = this.getSVGPathData();
+    if (!pathData.commands.length) {
+      return [];
+    }
+
+    return pathData.commands.flatMap(command => this.getShapesForCommand(command));
+  }
+
+  getSVGPathData(): SVGPathData {
+    return new SVGPathData(this.path)
       // Transforms relative commands to absolute so we don't have to implement relative commands at all!
       .toAbs()
       // Transforms S and T commands to C and Q so we don't have to implement S and T commands!
       .normalizeST()
       .transform(this.transformSize);
+  }
 
+  // TODO: Pass blankingAmount into wait.draw().
+  moveTo(command: CommandM): Wait {
+    this.lastMoveCommand = command;
+    this.prevX = command.x;
+    this.prevY = command.y;
+
+    return new Wait({
+      x: command.x,
+      y: command.y,
+      amount: this.blankingAmount
+    });
+  }
+
+  lineTo(command: CommandM | CommandL, blankAfter: boolean | undefined = undefined): Line {
+    const prevX = this.prevX;
+    const prevY = this.prevY;
+    const toX = 'x' in command ? command.x : prevX;
+    const toY = 'y' in command ? command.y : prevY;
+
+    this.prevX = toX;
+    this.prevY = toY;
+
+    const x = new Line({
+      from: { x: prevX, y: prevY },
+      to: { x: toX, y: toY },
+      color: this.color,
+      blankAfter: blankAfter,
+      waitAmount: this.waitAmount,
+      blankingAmount: this.blankingAmount
+    });
+    return x;
+  }
+
+  horizTo(command: CommandH): Line {
+    const lineCommand: CommandL = {
+      ...command,
+      y: this.prevY,
+      type: SVGPathData.LINE_TO
+    };
+    return this.lineTo(lineCommand);
+  }
+
+  vertTo(command: CommandV): Line {
+    const lineCommand: CommandL = {
+      ...command,
+      x: this.prevX,
+      type: SVGPathData.LINE_TO
+    };
+    return this.lineTo(lineCommand);
+  }
+
+  curveTo(command: CommandC): CubicCurve {
+    const prevX = this.prevX;
+    const prevY = this.prevY;
+
+    this.prevX = command.x;
+    this.prevY = command.y;
+
+    return new CubicCurve({
+      from: {
+        x: prevX,
+        y: prevY,
+        control: { x: command.x1, y: command.y1 }
+      },
+      to: {
+        x: command.x,
+        y: command.y,
+        control: { x: command.x2, y: command.y2 }
+      },
+      color: this.color
+    });
+  }
+
+  quadTo(command: CommandQ): QuadCurve {
+    const prevX = this.prevX;
+    const prevY = this.prevY;
+
+    this.prevX = command.x;
+    this.prevY = command.y;
+
+    return new QuadCurve({
+      from: { x: prevX, y: prevY },
+      to: { x: command.x, y: command.y },
+      control: { x: command.x1, y: command.y1 },
+      color: this.color
+    });
+  }
+
+  arcTo(command: CommandA): CubicCurve[] {
+    const prevX = this.prevX;
+    const prevY = this.prevY;
+
+    this.prevX = command.x;
+    this.prevY = command.y;
+
+    // TODO: Of course it would be better to implement this properly instead of converting arcs to a cubic bezier.
+    const curves = arcToBezier({
+      px: prevX,
+      py: prevY,
+      cx: command.x,
+      cy: command.y,
+      rx: command.rX,
+      ry: command.rY,
+      xAxisRotation: command.xRot,
+      largeArcFlag: command.lArcFlag,
+      sweepFlag: command.sweepFlag
+    });
+
+    let curvePrevX = prevX;
+    let curvePrevY = prevY;
+
+    return curves.map(curve => {
+      const cubic = new CubicCurve({
+        from: {
+          x: curvePrevX,
+          y: curvePrevY,
+          control: { x: curve.x1, y: curve.y1 }
+        },
+        to: {
+          x: curve.x,
+          y: curve.y,
+          control: { x: curve.x2, y: curve.y2 }
+        },
+        color: this.color
+      });
+
+      curvePrevX = curve.x;
+      curvePrevY = curve.y;
+
+      return cubic;
+    });
+  }
+
+  closePath(): Line {
+    if (!this.lastMoveCommand) {
+      throw new Error(
+        'Path parsing error: close path command called without a prior move command.'
+      );
+    }
+    return this.lineTo(this.lastMoveCommand, true);
+  }
+
+  getShapesForCommand(command: SVGCommand): Shape[] {
+    switch (command.type) {
+      case SVGPathData.MOVE_TO:
+        return [this.moveTo(command)];
+      case SVGPathData.HORIZ_LINE_TO:
+        return [this.horizTo(command)];
+      case SVGPathData.VERT_LINE_TO:
+        return [this.vertTo(command)];
+      case SVGPathData.LINE_TO:
+        return [this.lineTo(command)];
+      case SVGPathData.CURVE_TO:
+        return [this.curveTo(command)];
+      case SVGPathData.QUAD_TO:
+        return [this.quadTo(command)];
+      case SVGPathData.ARC:
+        return this.arcTo(command);
+      case SVGPathData.CLOSE_PATH:
+        return [this.closePath()];
+      default:
+        console.warn(
+          `Path parsing warning: command ${command.type} is not supported`
+        );
+        return [];
+    }
+  }
+
+  /**
+   * Get the relative angle at p2 when a line goes from
+   * p1 -> p2 -> p3
+   * 0 is a straight line
+   * .5 is 90 degrees
+   * 1 is 180 degrees
+   * This returns the absolute value, so it should only return from 0 to 1.
+   */
+  getRelativeAngle(p1: Point, p2: Point, p3: Point): number {
+    // Get previous angle in radians.
+    const previousAngle = Math.atan2(
+      p1.y - p2.y,
+      p1.x - p2.x
+    );
+
+    // Get next angle in radians.
+    const nextAngle = Math.atan2(
+      p2.y - p3.y,
+      p2.x - p3.x
+    );
+
+    // Get difference in angle where 90 degrees is 0.5, and 180 is 1.
+    return Math.abs(
+      Math.atan2(
+        Math.sin(previousAngle - nextAngle),
+        Math.cos(previousAngle - nextAngle)
+      )
+    ) / Math.PI;
+  }
+
+  draw(options: SceneOptions): Point[] {
+    const reducer = (accumulator: Point[], shape: Shape) => {
+      const commandPoints = shape.draw(options);
+      let waitPoints: Point[] = [];
+      if (accumulator.length >= 2 && commandPoints.length) {
+        const nextPoint: Point = commandPoints[0];
+        const lastPoint: Point = accumulator[accumulator.length - 1];
+        const secondLastPoint: Point = accumulator[accumulator.length - 2];
+
+        // I'm not positive this is the right factor to use for
+        // the wait time, but it works well enough most of the time.
+        const relativeAngle = this.getRelativeAngle(secondLastPoint, lastPoint, nextPoint);
+
+        const waitShape = new Wait({
+          x: lastPoint.x,
+          y: lastPoint.y,
+          color: [lastPoint.r, lastPoint.g, lastPoint.b],
+          fractionOfMax: relativeAngle
+        });
+        waitPoints = waitShape.draw(options);
+      }
+
+      return accumulator.concat(waitPoints, commandPoints);
+    };
+    return this.getPathShapes().reduce(reducer, []);
+    // It would be nice if we could flatMap but I'm not sure that will
+    // be enough info to calculate wait times yet.
+    //return this.getPathShapes().flatMap(shape => shape.draw(options));
+  }
+
+  drawOld(options: SceneOptions): Point[] {
+    const pathData = this.getSVGPathData();
     if (!pathData.commands.length) {
       return [];
     }
